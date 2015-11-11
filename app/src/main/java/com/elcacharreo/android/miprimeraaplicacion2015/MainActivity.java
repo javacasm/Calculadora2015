935330f56465bc2e1bdee52b681e24aecd33d43a
javacasm <javacasm@gmail.com>
2015-11-11 18:25:47 +0100

    margen=30

--- app/src/main/java/com/elcacharreo/android/miprimeraaplicacion2015/MainActivity.java
+++ app/src/main/java/com/elcacharreo/android/miprimeraaplicacion2015/MainActivity.java
@@ -2,6 +2,9 @@ package com.elcacharreo.android.miprimeraaplicacion2015;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void teclanumerica(View v)
+    {
+
+        Button botonPulsado= (Button)v;  // Casting
+
+        String strTextoBoton=botonPulsado.getText().toString();
+        String strMensajeInicial="Has pulsado la tecla: ";
+        String strMensaje=strMensajeInicial+strTextoBoton;
+
+        Toast.makeText(this,strMensaje,Toast.LENGTH_LONG).show();
+    }
+
 }
