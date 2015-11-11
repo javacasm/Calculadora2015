c30c3f23be4516d0a4c56dce05b514d5a3227b30
javacasm <javacasm@gmail.com>
2015-11-11 19:59:04 +0100

    margen=30

--- app/src/main/java/com/elcacharreo/android/miprimeraaplicacion2015/MainActivity.java
+++ app/src/main/java/com/elcacharreo/android/miprimeraaplicacion2015/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,6 +25,41 @@ public class MainActivity extends AppCompatActivity {
         String strMensaje=strMensajeInicial+strTextoBoton;
 
         Toast.makeText(this,strMensaje,Toast.LENGTH_LONG).show();
+
+
+
+        TextView tvVisor=(TextView)findViewById(R.id.TextView);
+
+        String strValorAntiguo=tvVisor.getText().toString();
+        if(strValorAntiguo.equals("0"))
+        {strValorAntiguo=""; }
+        String strValorNuevo=strValorAntiguo+strTextoBoton;
+
+
+        try {
+            float operando1 = Float.valueOf(strValorNuevo);
+
+            tvVisor.setText(strValorNuevo);
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this,"Tecla incorrecta:"+strTextoBoton,Toast.LENGTH_LONG).show();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 }
